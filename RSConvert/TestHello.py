e8faecfe986ba37557a37b4553b780c5fb4fd9ec
Yian Wang <yian@datamesh.com>
2018-05-28 20:02:21 +0800

    a

--- RSConvert/TestHello.py
+++ RSConvert/TestHello.py
@@ -4,6 +4,9 @@ import hou
 def say_hello():
     print 'hello'
     #result = hou.node('/obj').createNode("geo")
-
+    for node in hou.selectedNodes():
+        print('in'+node.path()+':')
+        for child in node.children():
+            print(child.path()+', type:'+child.type().name())
 
 say_hello()
